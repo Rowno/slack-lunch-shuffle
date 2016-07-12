9054a79da3f4e5a38eaac2fec1a7fd4da8d429b5
Roland Warmerdam <roland@segment.com>
2016-07-11 20:30:23 -0700

    command: send the start response async so the command isn't outputted
    Also fix the slack desktop notification content

--- app/command.js
+++ app/command.js
@@ -1,38 +1,41 @@
 'use strict';
+const got = require('got');
 const config = require('./config');
 
 
-function start() {
-    return {
-        response_type: 'in_channel',
-        attachments: [
-            {
-                fallback: "You're unable to join the shuffle",
-                text: "Would you like to join today's Lunch Shuffle?",
+function start(url) {
+    return got.post(url, {
+        timeout: 5000,
+        body: JSON.stringify({
+            response_type: 'in_channel',
+            text: "Would you like to join today's Lunch Shuffle?",
+            attachments: [{
+                fallback: "You're unable to join the lunch shuffle",
                 callback_id: 'join',
-                actions: [
-                    {
-                        name: 'join',
-                        text: 'Yes! 😎',
-                        type: 'button',
-                        style: 'primary'
-                    },
-                    {
-                        name: 'leave',
-                        text: 'No 😞',
-                        type: 'button',
-                        style: 'danger',
-                        confirm: {
-                            title: 'Are you sure?',
-                            text: 'Your awesome colleagues will be disappointed...',
-                            ok_text: 'Yes',
-                            dismiss_text: 'No'
-                        }
+                actions: [{
+                    name: 'yes',
+                    text: 'Yes! 😎',
+                    type: 'button',
+                    style: 'primary',
+                }, {
+                    name: 'no',
+                    text: 'No 😞',
+                    type: 'button',
+                    style: 'danger',
+                    confirm: {
+                        title: 'Are you sure?',
+                        text: 'Your awesome colleagues will be disappointed...',
+                        ok_text: 'Yes',
+                        dismiss_text: 'No',
                     }
-                ]
-            }
-        ]
-    };
+                }]
+            }]
+        }),
+    }).then((response) => {
+        console.log(response.body);
+    }).catch((error) => {
+        console.error(error);
+    });
 }
 
 function *route() {
@@ -46,7 +49,8 @@ function *route() {
     console.log(this.request.body);
 
     if (subcommand === 'start') {
-        this.body = start();
+        this.body = '';
+        start(this.request.body.response_url);
     } else if (subcommand === 'cancel') {
         this.body = 'Cancelling the lunch shuffle...';
     } else {
